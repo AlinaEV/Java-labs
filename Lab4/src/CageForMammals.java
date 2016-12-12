534b6feb5e6709532346bccd3d53b7e1ad3041ff
AlinaEV <alvityuk@gmail.com>
2016-12-12 15:11:13 +0200

    fixed

--- Lab4/src/CageForMammals.java
+++ Lab4/src/CageForMammals.java
@@ -1,6 +1,6 @@
 /**
  * Created by Алина on 06.11.2016.
  */
-public class CageForMammals<M extends Mammal> extends Cage<Mammal>{
+public class CageForMammals<M extends Mammal> extends Cage<M>{
 
 }
